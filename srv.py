ab59dc25caf21f133fe31278403095e0f986112d
sirefab <krzfabian@gmail.com>
2018-05-16 17:59:30 +0200

    config and structure

--- srv.py
+++ srv.py
@@ -1,16 +1,36 @@
+from functools import partial
+import websockets
 from aiohttp import web
 import asyncio
+# Dies ist ein Kommentar. Überrascht?
 
+loop = asyncio.get_event_loop()  # Eine AbstractEventLoop
+app = web.Application(loop=loop)  # web.Application
 
-loop = asyncio.get_event_loop()
-app = web.Application(loop=loop)
-handler = app.make_handler()
-future = loop.create_server(handler, "localhost", 8080)
-loop.run_until_complete(future)
-
-router = app.router
-
+router = app.router  # Setze "router" auf "app.router"
+async def handle_hello(req):  # Definiere eine Coro
+    return web.Response(text="Hello")  # Gib "Hello" zurück
 router.add_route("GET", "/test", handle_hello)
 
-async def handle_hello(req):
-    return web.Response("Hello")
+async def stop(req):
+    exit(1)
+router.add_route("GET", "/close", stop)  # Erstelle Route "close"
+
+async def fabian(req): # test
+    file = open("./webapp/fabian.html", "rb").readlines()
+    return web.Response(text=file, content_type="text/html")
+router.add_route("GET", "/fabian", fabian) # WOOW PARTIALS!
+
+handler = app.make_handler()  # Erstelle einen Handler
+future = loop.create_server(handler, "localhost", 8080)  # EIN SERVER!
+
+loop.create_task(future)  # Starte den Server
+ 
+async def handle_ws(ws, path):
+    async for message in ws:
+        print(message)
+        await ws.send("HELLO")
+
+loop.run_until_complete(websockets.serve(handle_ws, "localhost", 8765))  # Lasse den WS-Server laufen, bis er fertig ist. *Ist er nie* ;) ;) ;)
+
+loop.run_forever()  # Lasse die Loop für immer laufen
